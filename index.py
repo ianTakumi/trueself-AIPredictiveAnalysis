9c212ddbf50ba1036e65fbc7dffa6c29d604e1f0
Ian Calica <Iggc654@gmail.com>
2025-02-12 07:58:57 +0800

    Updated

--- index.py
+++ index.py
@@ -2,19 +2,28 @@ from flask import Flask, request, jsonify
 import pandas as pd
 import joblib
 from tensorflow.keras.models import load_model
+from flask_cors import CORS 
 import numpy as np
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb+srv://trueself:trueself@cluster0.ytknj.mongodb.net/trueselfDB?retryWrites=true&w=majority&appName=Cluster0"
+mongo = PyMongo(app)
 
+CORS(app)
 @app.route('/')
 def home():
-    return "Hello, Flask!"
-
-@app.route('/predict', methods=['POST'])
-def predict():
     try:
-        
+        collections = mongo.db.list_collection_names()
+        return jsonify({"message": "Successfully runs and  connected to MongoDB!", "collections": collections}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+@app.route('/predict/<user_id>', methods=['POST'])
+def predict(user_id):
+    try:
         data = request.get_json()
+        
         # Load the model and scaler
         model = load_model("anxiety_model.h5") 
         scaler = joblib.load("scaler.pkl")  
@@ -43,6 +52,34 @@ def predict():
         
         # Make a prediction
         predicted_severity = model.predict(X_test_scaled)
+        
+         # Extract occupation field that is 1
+        occupation_fields = ['Occupation_Engineer', 'Occupation_Other', 'Occupation_Student', 
+                             'Occupation_Teacher', 'Occupation_Unemployed']
+        
+        user_occupation = next((field.replace("Occupation_", "") for field in occupation_fields if data.get(field) == 1), None)
+        
+        mongo.db.anxietyPredictions.insert_one({
+            'userId': user_id,
+            "severityScore": round(float(predicted_severity[0][0]), 2),
+            "age": data['Age'],
+            "sleepHours": data['Sleep Hours'],
+            "physicalActivity": data['Physical Activity (hrs/week)'],
+            "caffeineIntake": data['Caffeine Intake (mg/day)'],
+            "alcoholConsumption": data['Alcohol Consumption (drinks/week)'],
+            "smoking": data['Smoking'],
+            "familyHistory": data['Family History of Anxiety'],
+            "stressLevel": data['Stress Level (1-10)'],
+            "heartRate": data['Heart Rate (bpm during attack)'],
+            "breathingRate": data['Breathing Rate (breaths/min)'],
+            "sweatingLevel": data['Sweating Level (1-5)'],
+            "dizziness": data['Dizziness'],
+            "medication": data['Medication'],
+            "therapySessions": data['Therapy Sessions (per month)'],
+            "recentMajorLifeEvent": data['Recent Major Life Event'],
+            "dietQuality": data['Diet Quality (1-10)'], 
+            "occupation": user_occupation if user_occupation else "Unknown"
+        })
         
         return jsonify({
             'predicted_severity': round(float(predicted_severity[0][0]), 2)
